0eacc2dcaf271300c2b17af156c4372664159654
KadPol <83218810+KadPol@users.noreply.github.com>
2021-09-28 09:36:44 +0200

    Add files via upload

--- scripts/postProcess.py
+++ scripts/postProcess.py
@@ -18,16 +18,3 @@ def nse(ypred, ytest):
     nse = 1 - (zahlersum/nennersum)
     return nse
 
-def modelperformance(results,index):
-    df = pd.DataFrame(results,index=index)
-
-    y1=[]
-    y2=[]
-    y3=[]
-    for i in df[0]:
-        y1.append(round(i,3))
-    for i in df[1]:
-        y2.append(round(i,3))
-    for i in df[2]:
-        y3.append(round(i,3))
-    return y1,y2,y3
