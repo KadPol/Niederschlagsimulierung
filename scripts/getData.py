e744d21e38bb42b54e00d638a9639321c990c102
KadPol <83218810+KadPol@users.noreply.github.com>
2021-09-28 16:00:16 +0200

    Add files via upload

--- scripts/getData.py
+++ scripts/getData.py
@@ -4,47 +4,42 @@ import numpy as np
 import datetime
 
 
-def cnn_lstm_data(areas,startyear, endyear):
-    wholeData=[]
-    wholeStreamflow=[]
-    for area in areas:
-        prcppath = './data_daymet/{}/{}_{}_prcp.nc'.format(area,area,startyear)
-        shape=Dataset(prcppath,'r')
-        data=np.empty((0,shape.variables['prcp'][:].shape[0],shape.variables['prcp'][:].shape[1],shape.variables['prcp'][:].shape[2],5))
-        streamflow_data= np.empty((0,365))
-        streamflow_path= './data_daymet/{}_streamflow_qc.txt'.format(area)
-        streamflow = dataio.load_streamflow(streamflow_path)
-        streamflow['streamflow']=streamflow['streamflow']*0.028316846592
-        streamflow.drop(['qc_flag','gauge_id'], inplace=True, axis=1)
-        counter=1
-        for i in range(startyear,endyear):
-            path = './data_daymet/{}/{}_{}_prcp.nc'.format(area,area,i)
-            srad_path = './data_daymet/{}/{}_{}_srad.nc'.format(area,area,i)
-            tmin_path = './data_daymet/{}/{}_{}_tmin.nc'.format(area,area,i)
-            tmax_path = './data_daymet/{}/{}_{}_tmax.nc'.format(area,area,i)
-            vp_path = './data_daymet/{}/{}_{}_vp.nc'.format(area,area,i)
-            f=Dataset(path,'r')
-            f_srad=Dataset(srad_path,'r')
-            f_tmin=Dataset(tmin_path,'r')
-            f_tmax=Dataset(tmax_path,'r')
-            f_vp=Dataset(vp_path,'r')
-            prcp = np.reshape(f.variables['prcp'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
-            srad = np.reshape(f_srad.variables['srad'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
-            tmin = np.reshape(f_tmin.variables['tmin'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
-            tmax = np.reshape(f_tmax.variables['tmax'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
-            vp = np.reshape(f_vp.variables['vp'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
-            prcp = np.append(prcp,srad,axis=4)
-            prcp = np.append(prcp,tmax,axis=4)
-            prcp = np.append(prcp,tmin,axis=4)
-            prcp = np.append(prcp,vp,axis=4)
-            data= np.append(data,prcp,axis=0)
-            year='{}'.format(i)
-            streamflow_year= np.array(streamflow['streamflow'][year][0:365])
-            streamflow_year=np.reshape(streamflow_year,(1,len(streamflow_year)))
-            streamflow_data =np.append(streamflow_data,streamflow_year,axis=0)
-        wholeData.append(data)
-        wholeStreamflow.append(streamflow_data)
-    return wholeData,wholeStreamflow
+def cnn_lstm_data(area,startyear, endyear):
+    prcppath = './data_daymet/{}/{}_{}_prcp.nc'.format(area,area,startyear)
+    shape=Dataset(prcppath,'r')
+    data=np.empty((0,shape.variables['prcp'][:].shape[0],shape.variables['prcp'][:].shape[1],shape.variables['prcp'][:].shape[2],5))
+    streamflow_data= np.empty((0,365))
+    streamflow_path= './data_daymet/{}_streamflow_qc.txt'.format(area)
+    streamflow = dataio.load_streamflow(streamflow_path)
+    streamflow['streamflow']=streamflow['streamflow']*0.028316846592
+    streamflow.drop(['qc_flag','gauge_id'], inplace=True, axis=1)
+    counter=1
+    for i in range(startyear,endyear):
+        path = './data_daymet/{}/{}_{}_prcp.nc'.format(area,area,i)
+        srad_path = './data_daymet/{}/{}_{}_srad.nc'.format(area,area,i)
+        tmin_path = './data_daymet/{}/{}_{}_tmin.nc'.format(area,area,i)
+        tmax_path = './data_daymet/{}/{}_{}_tmax.nc'.format(area,area,i)
+        vp_path = './data_daymet/{}/{}_{}_vp.nc'.format(area,area,i)
+        f=Dataset(path,'r')
+        f_srad=Dataset(srad_path,'r')
+        f_tmin=Dataset(tmin_path,'r')
+        f_tmax=Dataset(tmax_path,'r')
+        f_vp=Dataset(vp_path,'r')
+        prcp = np.reshape(f.variables['prcp'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
+        srad = np.reshape(f_srad.variables['srad'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
+        tmin = np.reshape(f_tmin.variables['tmin'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
+        tmax = np.reshape(f_tmax.variables['tmax'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
+        vp = np.reshape(f_vp.variables['vp'][:], (counter,f.variables['prcp'][:].shape[0], f.variables['prcp'][:].shape[1], f.variables['prcp'][:].shape[2],1))
+        prcp = np.append(prcp,srad,axis=4)
+        prcp = np.append(prcp,tmax,axis=4)
+        prcp = np.append(prcp,tmin,axis=4)
+        prcp = np.append(prcp,vp,axis=4)
+        data= np.append(data,prcp,axis=0)
+        year='{}'.format(i)
+        streamflow_year= np.array(streamflow['streamflow'][year][0:365])
+        streamflow_year=np.reshape(streamflow_year,(1,len(streamflow_year)))
+        streamflow_data =np.append(streamflow_data,streamflow_year,axis=0)
+    return data,streamflow_data
 
 
 def lstm_data(basin,start_date, end_date,days):
