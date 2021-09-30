6204fd334d6db7ba8bc60e71763d7b548feb6398
KadPol <83218810+KadPol@users.noreply.github.com>
2021-09-30 22:04:20 +0200

    Add files via upload

--- scripts/model.py
+++ scripts/model.py
@@ -25,22 +25,18 @@ def loss(history: tf.keras.callbacks.History):
 
 def model_LSTM(days,features):
     return tf.keras.Sequential([
-    tf.keras.layers.LSTM(50,  input_shape=(days,features), return_sequences=True),
-    tf.keras.layers.LSTM(30, return_sequences=True),
-    tf.keras.layers.LSTM(10),
+    tf.keras.layers.LSTM(30, return_sequences=True,dropout=0.2),
+    tf.keras.layers.LSTM(10,dropout=0.2),
     tf.keras.layers.Dense(1)
 ])
 
 def model_CNN_LSTM(days,x_shape,y_shape,features):
     return tf.keras.Sequential([
-    tf.keras.layers.TimeDistributed(
-        tf.keras.layers.Conv2D(64,(3,3),strides=2,input_shape=(days,x_shape,y_shape,features))),
-    tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D(4)),
-    tf.keras.layers.TimeDistributed(
-        tf.keras.layers.Conv2D(32,(2,2),strides=2)),
-    tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D(2)),
-    tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten()),
-    tf.keras.layers.LSTM(units=30,dropout=0.2,return_sequences=True),
-    tf.keras.layers.LSTM(units=10,dropout=0.1),
-    tf.keras.layers.Dense(units=1)
-])
+        tf.keras.layers.TimeDistributed(
+            tf.keras.layers.Conv2D(8,(3,3),strides=2,input_shape=(days,x_shape,y_shape,features))),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D(4)),
+        tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten()),
+        tf.keras.layers.LSTM(units=10,dropout=0.2),
+        tf.keras.layers.Dense(units=1)
+    ])
